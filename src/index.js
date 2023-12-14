2c84fb4a2dec2cc9bc6865a508b4cfae9d3c6aeb
rick <dxq7541@gmail.com>
2023-12-13 23:25:26 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.scss'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { Provider } from 'react-redux'
+import store from './store'
+import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 );
